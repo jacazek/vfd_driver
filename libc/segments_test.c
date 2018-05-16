f9fbe1883356217719863a28e96292cbd1e1a0b2
jacazek <jacobjwheeler@gmail.com>
2018-05-15 20:27:37 -0400

    feature: merge active segments from all digits

--- libc/segments_test.c
+++ libc/segments_test.c
@@ -26,9 +26,33 @@ void GivenA4DigitNumber_WhenConvertToSegments_ThenArrayIsPopulatedWithAllDigits(
 	TEST_ASSERT_EQUAL_UINT8_ARRAY(expectedOutput, output, 4);
 }
 
+void GivenSomeSegemnts_WhenMergeSegments_AllSegmentBitsORed() {
+	segment input[2] = { 1, 1<<3 };
+	TEST_ASSERT_EQUAL_UINT8(9, mergeSegments(input, 2));
+}
+
+void GivenNoSegments_WhenMergeSegments_AllBitsAreZero() {
+	segment input[0];
+	TEST_ASSERT_EQUAL_UINT8(0, mergeSegments(input, 0));
+}
+
+void Given2SegmentsThatAreZero_WhenMergeSegments_AllBitsAreZero() {
+	segment input[2] = { 0, 0 };
+	TEST_ASSERT_EQUAL_UINT8(0, mergeSegments(input, 2));
+}
+
+void GivenAllOnes_AndAllZeros_WhenMergeSegments_AllBitsAreOne() {
+	segment input[2] = { 255, 0 };
+	TEST_ASSERT_EQUAL_UINT8(255, mergeSegments(input, 2));
+}
+
 int main() {
 	UNITY_BEGIN();
 	RUN_TEST(GivenANumber_WhenConvertToSegments_ThenArrayIsPopulated);
 	RUN_TEST(GivenA4DigitNumber_WhenConvertToSegments_ThenArrayIsPopulatedWithAllDigits);
+	RUN_TEST(GivenSomeSegemnts_WhenMergeSegments_AllSegmentBitsORed);
+	RUN_TEST(GivenNoSegments_WhenMergeSegments_AllBitsAreZero);
+	RUN_TEST(Given2SegmentsThatAreZero_WhenMergeSegments_AllBitsAreZero);
+	RUN_TEST(GivenAllOnes_AndAllZeros_WhenMergeSegments_AllBitsAreOne);
 	return UNITY_END();
 };
